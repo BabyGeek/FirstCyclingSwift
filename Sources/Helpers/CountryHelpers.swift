cf961203779abe6ce5d01a8fbdf01dec04b38f20
Paul OGGERO <paul.oggero@itgrate.com>
2024-04-27 10:56:07 +0700

    🧑‍💻 Set up contants and helpers

---
+++ Sources/Helpers/CountryHelpers.swift
@@ -0,0 +1,22 @@
+//
+//  CountryHelpers.swift
+//
+//
+//  Created by Paul Oggero on 27/4/24.
+//
+
+import Foundation
+
+struct CountryHelpers {
+    static func countryFlagEmoji(for countryCode: String?) -> String? {
+        guard let countryCode, countryCode.count == 2 else { return nil }
+        
+        let base: UInt32 = 127397
+        var flagString = ""
+        for scalar in countryCode.uppercased().unicodeScalars {
+            flagString.unicodeScalars.append(UnicodeScalar(base + scalar.value)!)
+        }
+        
+        return flagString
+    }
+}
