39b703e3d7209d17c1c3d6e379ec2d564a89e15f
Paul OGGERO <paul.oggero@itgrate.com>
2024-04-30 13:53:17 +0700

    🧪 (races): Add tests for races list resource

--- Tests/FirstCyclingSwiftTests/FirstCyclingRaceTests/FirstCyclingSwiftRaceListTests.swift
+++ Tests/FirstCyclingSwiftTests/FirstCyclingRaceTests/FirstCyclingSwiftRaceListTests.swift
@@ -6,30 +6,87 @@
 //
 
 import XCTest
+@testable import FirstCyclingSwift
 
 final class FirstCyclingSwiftRaceListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var raceProvider: FirstCyclingRace!
+    var mockDataLoader: MockDataLoader!
+    
+    override func setUp() {
+        super.setUp()
+        
+        mockDataLoader = MockDataLoader(mockData: [
+            URL(string: "https://firstcycling.com/race.php")!: "mockRaceListData",
+            URL(string: "https://firstcycling.com/race.php?y=2024&t=2")!: "mockRaceListFilterByYearAndTypeData",
+        ])
+        
+        raceProvider = FirstCyclingRace(urlDataLoader: mockDataLoader)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        mockDataLoader = nil
+        raceProvider = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testFetchingRaceList() async throws {
+        let raceList = try await raceProvider.fetchRaceList()
+        
+        XCTAssertNotNil(raceList, "Race list should not be nil")
+        XCTAssertFalse(raceList.isEmpty, "Race list should not be empty")
+        
+        let expectedFirstRace = FirstCyclingRace(
+            id: 1,
+            dateRange: "16.01-21.01",
+            name: "Tour Down Under",
+            category: "2.UWT",
+            flag: "🇦🇺",
+            winner: FirstCyclingRider(
+                id: 37376,
+                name: "S. Williams",
+                flag: "🇬🇧"
+            )
+        )
+        
+        XCTAssertEqual(raceList.first, expectedFirstRace, "First race should equal the expected race")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testFetchingRaceListWithParameters() async throws {
+        let parameters = RaceListQueryParameters(
+            type: RaceType(name: "UCI", value: 2),
+            year: "2024",
+        )
+        
+        let raceList = try await raceProvider.fetchRaceList(withParameters: parameters)
+        
+        XCTAssertNotNil(raceList, "Race list should not be nil")
+        XCTAssertFalse(raceList.isEmpty, "Race list should not be empty")
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM"
+        let expectedDateString = "01.04"
+        guard let expectedDate = dateFormatter.date(from: expectedDateString) else {
+            XCTFail("Failed to create date from expected date string")
+            return
         }
+        
+        
+        
+        let expectedFirstRace = FirstCyclingRace(
+            id: 295,
+            dateRange: "01.04",
+            name: "Giro del Belvedere",
+            category: "1.2U",
+            flag: "🇮🇹",
+            winner: FirstCyclingRider(
+                id: 114583,
+                name: "G. Glivar",
+                flag: "🇸🇮"
+            )
+        )
+        
+        XCTAssertEqual(raceList.first, expectedFirstRace, "First race should be equal to the expected first race")
     }
 
 }
