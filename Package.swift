30f017cf9f1f44f5377979c3ce8a4120905173c6
Paul OGGERO <paul.oggero@itgrate.com>
2024-05-02 08:48:27 +0700

    🔧 Update Package description

--- Package.swift
+++ Package.swift
@@ -6,14 +6,13 @@ import PackageDescription
 let package = Package(
     name: "FirstCyclingSwift",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
         .watchOS(.v8),
         .macOS(.v11),
         .macCatalyst(.v14),
         .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FirstCyclingSwift",
             targets: ["FirstCyclingSwift"]),
@@ -22,12 +21,14 @@ let package = Package(
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FirstCyclingSwift", dependencies: ["SwiftSoup"]),
+            name: "FirstCyclingSwift", 
+            dependencies: ["SwiftSoup"]
+        ),
         .testTarget(
             name: "FirstCyclingSwiftTests",
-            dependencies: ["FirstCyclingSwift"]),
+            dependencies: ["FirstCyclingSwift"],
+            exclude: ["Resources/Mocks"]
+        )
     ]
 )
