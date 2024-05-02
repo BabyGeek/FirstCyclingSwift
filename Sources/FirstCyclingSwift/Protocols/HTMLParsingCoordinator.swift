e3061135ae212cce4c3fb866787d4da94ef31bd8
Paul OGGERO <paul.oggero@itgrate.com>
2024-05-02 08:56:50 +0700

    ✨ Add protocol for parsing coordinator

--- Sources/FirstCyclingSwift/Protocols/HTMLParsingCoordinator.swift
+++ Sources/FirstCyclingSwift/Protocols/HTMLParsingCoordinator.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-public protocol HTMLParsingCoordinator {
+internal protocol HTMLParsingCoordinator {
     func parseHTMLDataTable(_ htmlString: String) throws -> ParsedTable
 }
