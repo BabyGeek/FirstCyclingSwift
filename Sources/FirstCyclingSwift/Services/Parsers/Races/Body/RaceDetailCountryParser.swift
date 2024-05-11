a8759b63a8caa04af0342540896cc626cd94c7ff
Paul OGGERO <paul.oggero@itgrate.com>
2024-05-11 16:26:26 +0700

    ✅ (races): Update test and add mock data

--- Sources/FirstCyclingSwift/Services/Parsers/Races/Body/RaceDetailCountryParser.swift
+++ Sources/FirstCyclingSwift/Services/Parsers/Races/Body/RaceDetailCountryParser.swift
@@ -11,7 +11,7 @@ import SwiftSoup
 struct RaceDetailCountryNameParser: AdditionalInformationParser {
     func parseAdditionalInformations(fromHTML html: Element) throws -> [String: String] {
         if let h2Element = try html.select("h2").first() {
-            return ["country_name": try h2Element.text()]
+            return ["name": try h2Element.text()]
         }
         
         return [:]
--- Tests/FirstCyclingSwiftTests/FirstCyclingSwiftRaceTests/FirstCyclingSwiftRaceEditionTests/FirstCyclingSwiftRaceEditionResultsTests.swift
+++ Tests/FirstCyclingSwiftTests/FirstCyclingSwiftRaceTests/FirstCyclingSwiftRaceEditionTests/FirstCyclingSwiftRaceEditionResultsTests.swift
@@ -11,7 +11,7 @@ import XCTest
 final class FirstCyclingSwiftRaceEditionResultsTests: XCTestCase {
     var raceProvider: FirstCyclingRaceEndpointHandler!
     var mockDataLoader: MockDataLoader!
-    let raceID = 1234
+    let raceID = 13858
     let edition = 2024
     
     override func setUp() {
@@ -19,10 +19,6 @@ final class FirstCyclingSwiftRaceEditionResultsTests: XCTestCase {
         
         mockDataLoader = MockDataLoader(mockData: [
             "https://firstcycling.com/race.php?r=\(raceID)&y=\(edition)": .mockRaceEditionResultData,
-            "https://firstcycling.com/race.php?r=\(raceID)&y=\(edition)&l=2": .mockRaceEditionResultYouthData,
-            "https://firstcycling.com/race.php?r=\(raceID)&y=\(edition)&l=3": .mockRaceEditionResultPointsData,
-            "https://firstcycling.com/race.php?r=\(raceID)&y=\(edition)&l=4": .mockRaceEditionResultMountainData,
-            "https://firstcycling.com/race.php?r=\(raceID)&y=\(edition)&e=4": .mockRaceEditionResultStageData,
         ])
         
         
@@ -71,7 +67,7 @@ final class FirstCyclingSwiftRaceEditionResultsTests: XCTestCase {
     func testFetchingRaceEditionResults() async throws {
         let raceEdition = try await raceProvider.fetchRaceEdition(withID: raceID, edition: edition)
         
-        XCTAssertFalse(raceEdition.leaderboard, "Race edition leaderboard should not be empty")
+        XCTAssertFalse(raceEdition.leaderboard.isEmpty, "Race edition leaderboard should not be empty")
         
         let expectedFirstLeaderboardRanking = FirstCyclingRaceEditionRanking(
             position: 1,
@@ -87,7 +83,7 @@ final class FirstCyclingSwiftRaceEditionResultsTests: XCTestCase {
         
         
         let expectedLastLeaderboardRanking = FirstCyclingRaceEditionRanking(
-            position: 1,
+            position: -1,
             isDNF: true,
             rider: FirstCyclingRider(
                 id: 211336,
@@ -95,16 +91,21 @@ final class FirstCyclingSwiftRaceEditionResultsTests: XCTestCase {
                 team: "Thanh Hoa",
                 flag: "🇻🇳"
             ))
+        
+        XCTAssertEqual(raceEdition.leaderboard.last, expectedLastLeaderboardRanking, "First leaderboard rank should equal the expected ranking")
     }
     
     func checkRaceEditionAttributes(_ raceEdition: FirstCyclingRaceEdition) throws {
         XCTAssertEqual(raceEdition.name, "HTV Cup", "Race name should be HTV Cup")
         XCTAssertEqual(raceEdition.year, 2024, "Race edition should be 2024")
         
-        let expectedStartDate = "04/03/2024"
-        let expectedEndDate = "04/03/2024"
+        let expectedStartDate = "3 April 2024"
+        let expectedEndDate = "30 April 2024"
         
-        XCTAssertEqual(raceEdition.startDate, 2024, "Race edition should be 2024")
-        XCTAssertEqual(raceEdition.endDate, 2024, "Race edition should be 2024")
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMMM yyyy"
+        
+        XCTAssertEqual(raceEdition.startDate, dateFormatter.date(from: expectedStartDate), "Race edition should be 2024")
+        XCTAssertEqual(raceEdition.endDate, dateFormatter.date(from: expectedEndDate), "Race edition should be 2024")
     }
 }
